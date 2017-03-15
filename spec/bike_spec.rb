77f42d16b4dd77a1b7454d48fcbd98b9877e7aac
Alex Jukes <alexjukes@Alexs-MacBook-Pro-2.local>
2017-03-15 12:35:30 +0000

    Fixed bike test load error

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,8 @@
-require './bike.rb'
+require './lib/bike.rb'
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
+it 'should respond_to "working?" function.' do
+  expect(subject).to respond_to :working?
+end
+  #it { is_expected.to respond_to :working? }
 end
