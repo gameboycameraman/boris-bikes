1f49be308ba19b19b7678bbf6da72e79ae3da562
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-16 14:50:24 +0000

    Adds ex11 - bikes are docked now. Woop Woop

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -6,6 +6,16 @@ describe DockingStation do
   it "Checks bike is working" do
     bike = Bike.new
     expect(bike).to be_working
-    
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to :bike }
+
+  it "check bike is docked" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.dock(bike)).to eq bike    
+  end
+
 end
